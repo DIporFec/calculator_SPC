ac4706d0b16c09a9755ea624a87812ddd9cd1789
DIporFec <pkirill.kp@gmail.com>
2024-09-28 20:23:44 +0300

    Resolved merge conflicts

--- calculator.cpp
+++ calculator.cpp
@@ -3,19 +3,12 @@
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
-<<<<<<< HEAD
-
 }
-
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
-=======
 }
-
 int Calculator::Sub (double a, double b)
 {
 	return Add (a, -b);
->>>>>>> 4ad40a3b4572deee4efc716e7bd6e6e4fda46dcf
 }
->>>>>>> e0ea21b (add a multiplication operation)
