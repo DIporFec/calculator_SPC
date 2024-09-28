642edbd0e0a7565529d2378325b3cc7202e2079f
DIporFec <pkirill.kp@gmail.com>
2024-09-28 19:55:51 +0300

    Виправлення помилок перед cherry-pick

--- calculator.cpp
+++ calculator.cpp
@@ -1,12 +1,22 @@
 #include "calculator.h"
 
+//Hello
+
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
->>>>>>> 4ad40a3 (fix truncation error)
+
 }
 
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
+
+//IO-25 Petrenko Kyryl
+//Pershe zavdanya
+//Z predmeto Systemne Programuvannya
+//Upravlinnya versiyamy kodu cherez Git
+//Vykonuyu robotu v 28.09.2024
+//KPI
+//FICE
